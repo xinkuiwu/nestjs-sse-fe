2fa9a4a5887713d4c604d6d822a97cf115943c5b
wuxinkui <wuxinkui@git>
2024-01-03 16:20:50 +0800

    feat: sse-fe

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const eventSource = new EventSource('http://localhost:3000/stream3');
+    eventSource.onmessage = ({ data }) => {
+      console.log('New message===', JSON.parse(data));
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>hello</div>
   );
+
 }
 
 export default App;
